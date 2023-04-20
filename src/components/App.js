eb10743cf51c296a01386bf942765e6d9d36a2d6
Serah-Njenga <serah35.nn@gmail.com>
2023-04-20 09:02:42 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,52 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import logo from "../assets/logo.svg";
+import ArticleList from "./Articlelist";
+
+
 
 console.log(blogData);
 
 function App() {
+  const blogName= "Underreacted!";
+  const aboutText="A blog about learning React";
+
+  const posts = {
+    name: "Underreacted",
+    image: logo,
+    about: "A blog about learning React",
+    posts: [
+      {
+        id: 1,
+        title: "Components 101",
+        date: "December 15, 2020",
+        preview: "Setting up the building blocks of your site",
+        minutes: 5,
+      },
+      {
+        id: 2,
+        title: "React Data Flow",
+        date: "December 11, 2020",
+        preview: "Passing props is never passé",
+        minutes: 15,
+      },
+      {
+        id: 3,
+        title: "Function vs Class Components",
+        preview: "React, meet OOJS.",
+        minutes: 47,
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
+    <div>
+      <Header blogName={blogName} />
+      <About aboutText={aboutText} />
+      <ArticleList posts={posts.posts} />
+     </div>
   );
 }
 
